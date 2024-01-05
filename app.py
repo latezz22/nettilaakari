778ad79d1ceb01217c049d098623a66e746a9b70
latezz22 <57223851+latezz22@users.noreply.github.com>
2024-01-05 18:17:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
+#Tuodaan oleelliset kirjastot
+
 from urllib import response
 from flask import Flask, render_template, request, jsonify, make_response
 from bot import luo_vastaus
 
 
-app = Flask(__name__)
+app = Flask(__name__) 
+
+#Tuodaan Flaskille tietoon html tiedosto
 
 @app.get("/")
 def index():
     return render_template("index.html")
 
+#Alustetaan funktio tiedon välitykseen
+
 @app.post("/vastaa")
 def vastaa():
+
     teksti = request.get_json()
+    #Käytetään vastaus toisen python tiedoston kautta
     vastaus = luo_vastaus(teksti)
 
+    #Palautetaan selaimelle
     return make_response(jsonify(vastaus))
     
 
